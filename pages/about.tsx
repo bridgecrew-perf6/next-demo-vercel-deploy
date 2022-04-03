9be785c7c0c5719114dc72e7bc6888bc790ed57e
Victor Acuña Uribe <vc.acunauribe@gmail.com>
2022-04-02 23:03:04 -0400

    Pre-deploy Vercel

---
+++ pages/about.tsx
@@ -0,0 +1,33 @@
+import { ReactElement } from 'react'
+import Link from 'next/link'
+import { DarkLayout } from '../components/layouts/DarkLayout'
+import { MainLayout } from '../components/layouts/MainLayout'
+
+export default function About() {
+  return (
+    <>
+      <h1>About Page</h1>
+
+      <h1 className="title">
+        {/* Ir a <a href="/">Home</a> */}
+        Ir a <Link href="/">Home</Link>
+      </h1>
+
+      <p className="description">
+        Get started by editing{' '}
+        <code className="code">pages/about.jsx</code>
+      </p>
+    </>
+  )
+}
+
+
+About.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <MainLayout>
+      <DarkLayout>
+        {page}
+      </DarkLayout>
+    </MainLayout>
+  );
+}
